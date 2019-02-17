926bb801456f54e6edb31c00754792f2fe088f1e
Aidan Musnitzky <musnit@gmail.com>
2019-02-16 18:06:23 -0700

    add to watcher

--- src/devtools/subpanels/explorer.jsx
+++ src/devtools/subpanels/explorer.jsx
@@ -7,23 +7,67 @@ class Explorer extends Component {
   constructor(props, context) {
     super(props);
     this.state = {
-      selectedAddress: ''
+      selectedAddress: '',
+      contractAddress: '',
+      addressInput: '',
+      watching: false
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.startWatching = this.startWatching.bind(this);
+    this.stopWatching = this.stopWatching.bind(this);
+  }
+
+  handleChange(event) {
+    if(event.target.value === 'custom') {
+      this.setState({ custom: true, selectedAddress: 'custom', addressInput: '' });
+    }
+    else {
+      this.setState({ custom: false, selectedAddress: event.target.value });
+    }
+  }
+
+  handleInputChange(event) {
+    this.setState({ addressInput: event.target.value });
+  }
+
+  startWatching(event) {
+    if(this.state.selectedAddress === 'custom') {
+      this.setState({ watching: true, contractAddress: this.state.addressInput });
+    }
+    else {
+      this.setState({ watching: true, contractAddress: this.state.selectedAddress });
+    }
+  }
+
+  stopWatching(event) {
+    this.setState({ watching: false, contractAddress: '' });
   }
 
   render() {
     console.log(this.props.contracts)
-    const selectedContract = this.props.contracts[this.state.selectedContractIndex];
+
     return <div>
-      Select Address to watch:
-      <select value={this.state.selectedAddress} onChange={this.handleChange}>
+      {!this.state.watching && <span>Select Address to watch:</span>}
+      {!this.state.watching && <select disabled={this.state.watching} value={this.state.selectedAddress} onChange={this.handleChange}>
         <option disabled value=''>Select an address</option>
         {this.props.contracts.map((contract, index) => <option key={contract.address} value={contract.address}>
           {contract.address}
         </option>)}
-      </select>
+        <option value='custom'>Enter custom address</option>
+      </select>}
 
-      {this.props.selectedAddress && <WatchAddress address={this.props.selectedAddress} />}
+      {!this.state.watching && this.state.custom && <span>
+        Enter contract address:
+        <input disabled={this.state.watching} type="text" value={this.state.addressInput} onChange={this.handleInputChange} />
+        {!this.state.watching && this.state.selectedAddress && this.state.addressInput && <button onClick={this.startWatching}>
+          Start Watching
+        </button>}
+      </span>}
+      {this.state.watching && <button onClick={this.stopWatching}>
+        Stop Watching
+      </button>}
+      {this.state.contractAddress && <WatchAddress address={this.state.contractAddress} />}
     </div>;
   }
 }
